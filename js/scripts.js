226c59b8f2b620ab78c977564ac02c9368a59878
amandachacon <91325122+amandachacon@users.noreply.github.com>
2021-10-07 12:09:04 +0200

    Populated blank array with 5 pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
 let pokemonList = [
+    {name: 'Charmander', type: ['fire'], height:0.6},
+    {name: 'Pikachu', type: ['electric'], height:0.4},
+    {name: 'Dragonite', type: ['dragon', 'flying'], height:2.2},
+    {name: 'Charizard', type: ['fire', 'flying'], height:1.7},
+    {name: 'Vaporeon', type: ['water'], height:1}
   ];
