ca231360c391238ca909d9ee0b9cabf4264c7b99
amandachacon <91325122+amandachacon@users.noreply.github.com>
2021-10-11 10:33:45 +0200

    "for" loop that writes the names and heights of the pokemon on the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@ let pokemonList = [
     {name: 'Charizard', type: ['fire', 'flying'], height:1.7},
     {name: 'Vaporeon', type: ['water'], height:1}
   ];
+
+  
+ for (i=0; i<pokemonList.length; i++){
+  if (pokemonList[i].height >2) {
+  document.write(pokemonList[i].name+" (Height- "+pokemonList[i].height+")"+" -Wow, that's big!"+"<br>");
+ }else {
+  document.write (pokemonList[i].name+" (Height- "+pokemonList[i].height+")" +"<br>")
+ }}
