04e5a3f4ea92934503630366ba903f0c5cfea182
amandachacon <91325122+amandachacon@users.noreply.github.com>
2021-10-25 15:39:14 +0200

    Wrapped PokemonList in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
-let pokemonList = [
-    {name: 'Charmander', type: ['fire'], height:0.6},
-    {name: 'Pikachu', type: ['electric'], height:0.4},
-    {name: 'Dragonite', type: ['dragon', 'flying'], height:2.2},
-    {name: 'Charizard', type: ['fire', 'flying'], height:1.7},
-    {name: 'Vaporeon', type: ['water'], height:1}
-  ];
-
+let pokemonRepository = (function () {
+  let pokemonList = [
+      {name: 'Charmander', type: ['fire'], height:0.6},
+      {name: 'Pikachu', type: ['electric'], height:0.4},
+      {name: 'Dragonite', type: ['dragon', 'flying'], height:2.2},
+      {name: 'Charizard', type: ['fire', 'flying'], height:1.7},
+      {name: 'Vaporeon', type: ['water'], height:1}
+    ];
+    
+  function getAll() {
+      return pokemonList;
+  };
+  function add(item) {
+      return pokemonList.push(item);
+  };
   
-  pokemonList.forEach(function(pokemonList){
+  return {
+      getAll: getAll,
+      add: add,
+  };
+  }());
+   pokemonRepository.getAll().forEach(function(pokemonList){
     document.write(`${pokemonList.name} -(Height: ${pokemonList.height}) <br>`)
   });
