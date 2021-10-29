dab408ce0979ab5eafe52ebb6ce2f5ba550b2d65
amandachacon <91325122+amandachacon@users.noreply.github.com>
2021-10-29 12:39:30 +0200

    updated Pokemon Repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,41 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-      {name: 'Charmander', type: ['fire'], height:0.6},
-      {name: 'Pikachu', type: ['electric'], height:0.4},
-      {name: 'Dragonite', type: ['dragon', 'flying'], height:2.2},
-      {name: 'Charizard', type: ['fire', 'flying'], height:1.7},
-      {name: 'Vaporeon', type: ['water'], height:1}
-    ];
-    
+  let repository = [
+    { name: 'Charmander', type: ['fire'], height: 0.6 },
+    { name: 'Pikachu', type: ['electric'], height: 0.4 },
+    { name: 'Dragonite', type: ['dragon', 'flying'], height: 2.2 },
+    { name: 'Charizard', type: ['fire', 'flying'], height: 1.7 },
+    { name: 'Vaporeon', type: ['water'], height: 1 }
+
+
+  ];
   function getAll() {
-      return pokemonList;
-  };
-  function add(item) {
-      return pokemonList.push(item);
-  };
-  
+    return repository;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon);
+    });
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
   return {
-      getAll: getAll,
-      add: add,
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
   };
-  }());
-   pokemonRepository.getAll().forEach(function(pokemonList){
-    document.write(`${pokemonList.name} -(Height: ${pokemonList.height}) <br>`)
-  });
+})();
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
