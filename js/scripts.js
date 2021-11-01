4ac6b9624c7196ac0a8b9cb5f09d38b22eb2e1ff
amandachacon <91325122+amandachacon@users.noreply.github.com>
2021-11-01 11:04:02 +0100

    Replace pokemon list with API

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 let pokemonRepository = (function () {
-  let repository = [
-    { name: 'Charmander', type: ['fire'], height: 0.6 },
-    { name: 'Pikachu', type: ['electric'], height: 0.4 },
-    { name: 'Dragonite', type: ['dragon', 'flying'], height: 2.2 },
-    { name: 'Charizard', type: ['fire', 'flying'], height: 1.7 },
-    { name: 'Vaporeon', type: ['water'], height: 1 }
-
-
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
