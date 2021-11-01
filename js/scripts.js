d11f2e9024c939cb779e9f4a6612a7e258493813
amandachacon <91325122+amandachacon@users.noreply.github.com>
2021-11-01 11:06:40 +0100

    JSON response

--- js/scripts.js
+++ js/scripts.js
@@ -20,12 +20,29 @@ let pokemonRepository = (function () {
     let listpokemon = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.addEventListener('click', function (event) {
-      showDetails(pokemon);
-    });
     button.classList.add("button-class");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
   function showDetails(pokemon) {
     console.log(pokemon.name);
