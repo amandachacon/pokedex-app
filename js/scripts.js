7ae6b41f1739ef7a3bfbdd726ba44ffd3c7d8307
amandachacon <91325122+amandachacon@users.noreply.github.com>
2021-11-19 15:09:19 +0100

    Removed unnecessary code
    modal doesn't pull height, image or type from API, added for code review.

--- js/scripts.js
+++ js/scripts.js
@@ -21,38 +21,15 @@ let pokemonRepository = (function () {
     let listpokemon = document.createElement('li');
     let button = document.createElement('button');
     let item = pokemon
-    //this breaks the page, no buttons appear, "li is not defined"
-    //li.classList.add('group-list-item');
     button.innerText = pokemon.name;
-    //breakes code (btn btn, primary), removed btn
     button.classList.add('btn-primary');
     button.setAttribute('data-toggle', 'modal');
     button.setAttribute('data-target', '#modal');
-
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
 
     button.addEventListener('click', () => {
       showDetails(pokemon);
-      showModal(pokemon);
-      console.log(pokemon)
-    });
-  }
-
-
-  function loadDetails(pokemon) {
-    let item = pokemon
-    let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      item.imageUrl = details.sprites.other['official-artwork']['front_default'];
-      item.imageUrlBack = details.sprites.back_default;
-      item.height = details.height;
-      item.types = details.types;
-      item.types = pokemon.types[0].type.name;
-    }).catch(function (e) {
-      console.error(e);
     });
   }
 
@@ -63,7 +40,7 @@ let pokemonRepository = (function () {
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
-          detailsUrl: item.detailsUrl
+          detailsUrl: item.Url
         };
         add(pokemon);
       });
@@ -72,68 +49,52 @@ let pokemonRepository = (function () {
     })
   }
 
-  function showDetails(item) {
-    loadDetails(item).then(function () {
-      showModal(item);
+  function loadDetails(pokemon) {
+    let item = pokemon
+    let detailsUrl = item.detailsUrl;
+    return fetch(detailsUrl).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrlFront = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
     });
   }
 
-  function showModal(item) {
+  function showDetails(pokemon) {
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      showModal(pokemon);
+    });
+  }
+
+  //changed item to pokemon (does name heigh string have anything to do with this?-nope breaks page)
+  function showModal(pokemon) {
     let modalBody = $(".modal-body");
     let modalTitle = $(".modal-title");
     let modalHeader = $(".modal-header");
-
+    let modalContainer = $(".modal");
+    let item = pokemon
     modalTitle.empty();
     modalBody.empty();
-    //dobble check element names in modal, consider adding back image back image code commented out
+    //consider adding back image back image code commented out
     let nameElement = $("<h1>" + item.name + "</h1>");
     let imageElementFront = $('<img class="modal-img" style="width:50%">');
-    imageElementFront.attr("src", item.imageUrl);
+    imageElementFront.attr("src", item.imageUrlFront);
     //let imageElementBack = $('<img class="modal-img" style="width:50%">');
     //imageElementBack.attr("src", item.ImageURLBack);
-    let heightElement = $("<p>" + "height" + item.height + "</p>");
+    let heightElement = $("<p>" + "height : " + item.height + "</p>");
     let typesElement = $("<p>" + "types : " + item.types + "</p>");
 
+
     modalTitle.append(nameElement);
-    modalBody.append(imageUrl);
-    //modalBody.append(imageElementBack);
+    modalBody.append(imageElementFront);
     modalBody.append(heightElement);
     modalBody.append(typesElement);
-    modalContainer.appendChild(modal);
 
   }
 
-
-  function hideModal() {
-    targetModal.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  targetModal.addEventListener('click', (e) => {
-    let target = e.target;
-    if (target === targetModal) {
-      hideModal();
-    }
-  });
-
-
-  function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-      showModal(item.name, item.height, item.imageUrl, item.types);
-      let pokemon = item
-      imgURL = pokemon.sprites.other['official-artwork']['front_default'];
-      let height = item.height;
-      let img = document.querySelector('img');
-      height.text = item.height
-      img.src = item.imgURL
-    });
-  }
- 
   return {
     add: add,
     getAll: getAll,
@@ -150,5 +111,3 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-
